9f79ccdcd52cfde8f29cc9e1b969c584cacc0ad2
sebesta2 <sebesta2@PC1049>
2024-03-19 12:59:50 +0100

    Add project files.

---
+++ BankApplication/DatabaseConnection/SqlDatabaseConnection.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication.DatabaseConnection
+{
+    public class SqlDatabaseConnection
+    {
+        private static SqlDatabaseConnection _instance;
+        public static SqlDatabaseConnection Instance
+        {
+            get
+            {
+                Config config = ConfigHandler.Config;
+                _instance ??= new SqlDatabaseConnection(config.IP, config.InitialCatalog, config.UID, config.Password);
+                return _instance;
+            }
+        }
+
+        private readonly string _connectionString;
+
+        private SqlDatabaseConnection(string ip, string databaseName, string UID, string pw)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = ip;
+            builder.InitialCatalog = databaseName;
+            builder.UserID = UID;
+            builder.Password = pw;
+            builder.Pooling = true;
+            _connectionString = builder.ToString();
+        }
+
+        #region Queries Methods
+        public int ExecuteNonQuery(string query, params SqlParameter[] parameters)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        if (parameters != null)
+                        {
+                            command.Parameters.AddRange(parameters);
+                        }
+
+                        return command.ExecuteNonQuery();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return 0;
+        }
+
+        public DataTable? ExecuteQuery(string query, params SqlParameter[] parameters)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        if (parameters != null)
+                        {
+                            command.Parameters.AddRange(parameters);
+                        }
+
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+                            return dataTable;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return null;
+        }
+
+        public int ExecuteScalarInt(string query, params SqlParameter[] parameters)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        if (parameters != null)
+                        {
+                            command.Parameters.AddRange(parameters);
+                        }
+
+                        object result = command.ExecuteScalar();
+
+                        if (result != null && int.TryParse(result.ToString(), out int intValue))
+                        {
+                            return intValue;
+                        }
+
+                        return 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return 0;
+        }
+
+        #endregion
+    }
+}
+}
